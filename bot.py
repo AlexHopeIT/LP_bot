872f4be106cf7f5b0c9139fd6f386dcb1e21577e
Alexey Schcennikov <hopefortomorrow@bk.ru>
2023-09-06 15:35:22 +0300

    Debag error

--- bot.py
+++ bot.py
@@ -1,27 +1,30 @@
 import logging
-from telegram.ext import updater, CommandHandler, MessageHandler, Filters
+import settings
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def greet_user(update, context):
     print('Вызван /start')
-    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+    update.message.reply_text('Привет, юзер! Ты вызвал команду /start')
 
 def talk_to_me(update, context):
     text = update.message.text
     print(text)
     update.message.reply_text(text)
 
-
 def main():
-    mybot = updater('6683903410:AAHBpMm1mSZD_53wuwT6Aot5FBElBdMRDKY', use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Бот стартовал')
     mybot.start_polling()
     mybot.idle()
 
-main()
+if __name__ == '__main__':
+    main()
+
